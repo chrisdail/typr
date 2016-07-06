84678b73a2dfc70b7cbd1884fa600936b1a2d056
Chris Dail <Christopher.Dail@emc.com>
2016-07-05 21:29:59 -0300

    Initial Commit

---
+++ Typr/KeyMonitor.swift
@@ -0,0 +1,75 @@
+//
+//  KeyMonitor.swift
+//  Typr
+//
+//  Created by Christopher Dail on 2016-07-02.
+//  Copyright © 2016 Christopher Dail. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class KeyMonitor {
+    var managedObjectContext: NSManagedObjectContext
+    var workspace: NSWorkspace!
+    var statusItem: NSStatusItem
+    
+    let characterSet = NSCharacterSet.alphanumericCharacterSet()
+    let whitespaceSet = NSCharacterSet.whitespaceAndNewlineCharacterSet()
+
+    var lastWord = ""    
+    var stats: WordStats
+    
+    
+    init(managedObjectContext: NSManagedObjectContext, statusItem: NSStatusItem) {
+        self.managedObjectContext = managedObjectContext
+        self.statusItem = statusItem
+        workspace = NSWorkspace.sharedWorkspace()
+        
+        stats = WordStats.findOrCreate(managedObjectContext)
+    }
+    
+    func handler(event: NSEvent) {
+        for uni in event.charactersIgnoringModifiers!.unicodeScalars {
+            // If we are a whitespace character AND last character was not whitespace
+            if whitespaceSet.longCharacterIsMember(uni.value) {
+                // Only count words with at least 2 characters
+                if lastWord.characters.count >= 2 || lastWord == "a" || lastWord == "i" {
+                    onWord(lastWord)
+                    lastWord = ""
+                }
+            }
+            else if characterSet.longCharacterIsMember(uni.value) {
+                lastWord.append(uni)
+            }
+        }
+        
+    }
+    
+    func onWord(word: String) {
+        // Get the current application with focus
+        let app = workspace.frontmostApplication
+        let appName = app!.localizedName!
+        
+        // Detect if we have changed days
+        if (!stats.isFromToday()) {
+            stats = WordStats.findOrCreate(managedObjectContext)
+        }
+        stats.recordNewWord(appName)
+        
+        statusItem.title = String(format: "%d", stats.total.intValue)
+    }
+    
+    // Get the current date without time information
+    func currentDate() -> NSDate {
+        let calendar = NSCalendar.currentCalendar()
+        let dateComponents = calendar.components([NSCalendarUnit.Day, NSCalendarUnit.Month,
+            NSCalendarUnit.Year, NSCalendarUnit.WeekOfYear], fromDate: NSDate())
+        return calendar.dateFromComponents(dateComponents)!
+    }
+    
+}
+
+// 51 backspace
+// 117 delete
+//
---
+++ Typr/WordStats.swift
@@ -0,0 +1,77 @@
+//
+//  WordStats.swift
+//  Typr
+//
+//  Created by Christopher Dail on 2016-07-03.
+//  Copyright © 2016 Christopher Dail. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class WordStats: NSManagedObject {
+
+    @NSManaged var total: NSNumber
+    @NSManaged var date: NSDate
+    @NSManaged var countByApp: NSDictionary
+
+    func recordNewWord(appName: String) {
+        var stats = countByApp as! [String: NSNumber]
+        
+        if let count = stats[appName] {
+            stats[appName] = NSNumber(int: count.intValue + 1)
+        }
+        else {
+            stats[appName] = NSNumber(int: 1)
+        }
+        countByApp = stats as NSDictionary
+        total = NSNumber(int: total.intValue + 1)
+        save()
+        print(countByApp)
+    }
+    
+    func save() {
+        do {
+            try self.managedObjectContext?.save()
+        } catch {
+            //let saveError = error as NSError
+            print(error)
+        }
+    }
+    
+    func isFromToday() -> Bool {
+        return WordStats.currentDate().isEqualToDate(date)
+    }
+    
+    class func findOrCreate(managedObjectContext: NSManagedObjectContext) -> WordStats {
+        // Try to find by the current date
+        let fetchRequest = NSFetchRequest(entityName: "WordStats")
+        
+        // Create Predicate
+        let predicate = NSPredicate(format: "%K == %@", "date", currentDate())
+        fetchRequest.predicate = predicate
+        
+        do {
+            if let stats = try managedObjectContext.executeFetchRequest(fetchRequest).first as? WordStats {
+                return stats
+            }
+        } catch {
+            //let fetchError = error as NSError
+            print(error)
+        }
+        
+        // Record not found, create a new one
+        let stats = NSEntityDescription.insertNewObjectForEntityForName("WordStats", inManagedObjectContext:     managedObjectContext) as! WordStats
+        stats.date = currentDate()
+        stats.total = NSNumber(int: 0)
+        return stats
+    }
+    
+    // Get the current date without time information
+    class func currentDate() -> NSDate {
+        let calendar = NSCalendar.currentCalendar()
+        let dateComponents = calendar.components([NSCalendarUnit.Day, NSCalendarUnit.Month,
+            NSCalendarUnit.Year, NSCalendarUnit.WeekOfYear], fromDate: NSDate())
+        return calendar.dateFromComponents(dateComponents)!
+    }
+}
